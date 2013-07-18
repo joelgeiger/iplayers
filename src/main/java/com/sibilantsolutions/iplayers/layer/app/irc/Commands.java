b84afe9b89df5a010e77fcd8698567e429b4f135
Joel Geiger <joel@sibilantsolutions.com>
2013-07-18 13:24:09 -0400

    Start work on processing commands.

---
+++ src/main/java/com/sibilantsolutions/iplayers/layer/app/irc/Commands.java
@@ -0,0 +1,28 @@
+package com.sibilantsolutions.iplayers.layer.app.irc;
+
+public enum Commands
+{
+    CMD_ERROR,
+    CMD_MODE,
+    CMD_NOTICE,
+    CMD_PING,
+
+    CMD_001,
+    CMD_002,
+    CMD_003,
+    CMD_004,
+    CMD_005,
+    CMD_042,
+    CMD_251,
+    CMD_252,
+    CMD_253,
+    CMD_254,
+    CMD_255,
+    CMD_265,
+    CMD_266,
+    CMD_372,
+    CMD_375,
+    CMD_376,
+    CMD_396,
+    ;
+}
--- src/main/java/com/sibilantsolutions/iplayers/layer/app/irc/IrcDataProc.java
+++ src/main/java/com/sibilantsolutions/iplayers/layer/app/irc/IrcDataProc.java
@@ -1,11 +1,15 @@
 package com.sibilantsolutions.iplayers.layer.app.irc;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import com.sibilantsolutions.iptools.event.ReceiveEvt;
 import com.sibilantsolutions.iptools.event.SocketListenerI;
 import com.sibilantsolutions.iplayers.layer.app.irc.domain.IrcLine;
 
 public class IrcDataProc implements SocketListenerI
 {
+    final static private Logger log = LoggerFactory.getLogger( IrcDataProc.class );
 
     @Override
     public void onReceive( ReceiveEvt evt )
@@ -14,5 +18,17 @@ public class IrcDataProc implements SocketListenerI
         String line = new String( evt.getData(), evt.getOffset(), evt.getLength() );
 
         IrcLine ircLine = IrcLineParser.parse( line );
+        //log.info( "prefix={}, command={}, parameters={}", ircLine.getPrefix(), ircLine.getCommand(), ircLine.getParameters() );
+
+        try
+        {
+            Commands command = Commands.valueOf( "CMD_" + ircLine.getCommand() );
+            //log.info( "Command={}.", command );
+        }
+        catch ( IllegalArgumentException e )
+        {
+            log.error( "Unexpected command=" + ircLine.getCommand(), e );
+        }
+
     }
 }

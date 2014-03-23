2157f3215c1161be0cf97719c7ebbee061bec319
Joel Geiger <joel@sibilantsolutions.com>
2014-03-23 12:00:35 -0400

    Add hex representations of values.

--- src/main/java/com/sibilantsolutions/iplayers/layer/app/tls/domain/HandshakeMessageType.java
+++ src/main/java/com/sibilantsolutions/iplayers/layer/app/tls/domain/HandshakeMessageType.java
@@ -16,17 +16,17 @@ public enum HandshakeMessageType
 
     static private class Values
     {
-        final static private int HelloRequest       = 0;
-        final static private int ClientHello        = 1;
-        final static private int ServerHello        = 2;
-        final static private int NewSessionTicket   = 4;
-        final static private int Certificate        = 11;
-        final static private int ServerKeyExchange  = 12;
-        final static private int CertificateRequest = 13;
-        final static private int ServerHelloDone    = 14;
-        final static private int CertificateVerify  = 15;
-        final static private int ClientKeyExchange  = 16;
-        final static private int Finished           = 20;
+        final static private int HelloRequest       = 0;    //0x00
+        final static private int ClientHello        = 1;    //0x01
+        final static private int ServerHello        = 2;    //0x02
+        final static private int NewSessionTicket   = 4;    //0x04
+        final static private int Certificate        = 11;   //0x0B
+        final static private int ServerKeyExchange  = 12;   //0x0C
+        final static private int CertificateRequest = 13;   //0x0D
+        final static private int ServerHelloDone    = 14;   //0x0E
+        final static private int CertificateVerify  = 15;   //0x0F
+        final static private int ClientKeyExchange  = 16;   //0x10
+        final static private int Finished           = 20;   //0x14
     }
 
     private int value;
